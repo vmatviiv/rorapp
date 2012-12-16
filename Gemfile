f6bec155788d205c18121db5acc7a0e5b5b87fa4
Volodymyr.Matviiv <volodymyr.matviiv@gmail.com>
2012-12-16 20:06:42 +0000

    Switched to MySQL database. Starting Ajax implementation.

--- Gemfile
+++ Gemfile
@@ -5,7 +5,10 @@ gem 'rails', '3.2.9'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+#gem 'sqlite3'
+gem 'mysql2', '~> 0.3.10'
+gem 'mysql'
+gem 'activerecord-mysql-adapter'
 
 gem 'execjs'
 gem 'therubyracer'
--- db/schema.rb
+++ db/schema.rb
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(:version => 20121208011231) do
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.decimal  "price",       :precision => 10, :scale => 0
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
   end
 
 end
